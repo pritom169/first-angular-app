8a34bc45dd556a73a530654b7fa0f911e5ef4056
Pritom Mazumder <pritommazumder@gmail.com>
2025-06-20 15:21:58 +0200

    [ADDITION] Initial components for users has been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true
 })
 export class AppComponent {
   title = 'first-angular-app';
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
+
 export class UserComponent {
 
 }
