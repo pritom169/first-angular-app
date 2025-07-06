76ef1b7c64d8731b5b1adeaed199948c631c5a13
Pritom Mazumder <pritommazumder@gmail.com>
2025-07-06 11:48:33 +0200

    [ADDITION] User Component and Card component has been transferred from standalsone component to module based component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { CardComponent } from './shared/card/card.component';
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent],
+    declarations: [AppComponent, HeaderComponent, UserComponent, CardComponent],
     bootstrap: [AppComponent],
-    imports: [BrowserModule, UserComponent, TasksComponent]
+    imports: [BrowserModule, TasksComponent]
 })
 
 export class AppModule {
--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card',
-  imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
+  standalone: false
 })
 export class CardComponent {
 
