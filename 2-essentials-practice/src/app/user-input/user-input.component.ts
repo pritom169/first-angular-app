ab1695a2777662582c89f3210dff590d07c231fc
Pritom Mazumder <pritommazumder@gmail.com>
2025-07-07 00:06:44 +0200

    [ADDITION] User component has been created and have been incorporated into the app

---
+++ 2-essentials-practice/src/app/user-input/user-input.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+  initialInvestment = 0;
+  annualInvestment = 0;
+  expectedReturn = 0;
+  duration = 0;
+}
