2926704a19ab1e23f6b7e5537340a006b4bab4c1
Pritom Mazumder <pritommazumder@gmail.com>
2025-07-07 23:28:02 +0200

    [ADDITION] Components seperated and template codes compartmentalized.

--- 3-cmp-deep-dive/src/app/app.component.ts
+++ 3-cmp-deep-dive/src/app/app.component.ts
@@ -1,41 +1,16 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { ServerStatusComponent } from './dashobard/server-status/server-status.component';
+import { TrafficComponent } from "./dashobard/traffic/traffic.component";
+import { TicketsComponent } from './dashobard/tickets/tickets.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html',
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent],
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
-  dummyTrafficData = [
-    {
-      id: 'd1',
-      value: 433,
-    },
-    {
-      id: 'd2',
-      value: 260,
-    },
-    {
-      id: 'd3',
-      value: 290,
-    },
-    {
-      id: 'd4',
-      value: 410,
-    },
-    {
-      id: 'd5',
-      value: 397,
-    },
-    {
-      id: 'd6',
-      value: 488,
-    },
-    {
-      id: 'd47',
-      value: 589,
-    },
-  ];
-  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
-  currentStatus = 'online';
+  
 }
