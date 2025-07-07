6802fa8a6106f34ca64bec7393be2be6d48ab8a7
Pritom Mazumder <pritommazumder@gmail.com>
2025-07-07 15:10:09 +0200

    [ADDITION] Final contribution project has been done.
    1. Unnecessar file `app.component.css` file has been removed
    2. All internal logics has been removed from app component and have been removed to investment result component.
    3. Enriched the data with Angular pip from common module

--- 2-essentials-practice/src/app/app.component.ts
+++ 2-essentials-practice/src/app/app.component.ts
@@ -1,20 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
-import { UserInput } from "./user-input/user-input.model";
 import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
   imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent]
 })
+
 export class AppComponent {
-  userInevestmentData?: UserInput;
   
-  onSubmitInvestmentData(userInput: UserInput): void {
-    this.userInevestmentData = userInput;
-  }
 }
--- 2-essentials-practice/src/app/investment-results/investment-results.component.ts
+++ 2-essentials-practice/src/app/investment-results/investment-results.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { InvesmentResult } from './investment-results.model';
 import { InvestmentResultsService } from './investment-results.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
