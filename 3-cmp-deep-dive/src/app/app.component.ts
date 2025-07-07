ea0769d067d8d566e3b38c03b7ee1eb9a2f41fcb
Pritom Mazumder <pritommazumder@gmail.com>
2025-07-08 00:18:37 +0200

    [ADDITION] Component codes has been separated

--- 3-cmp-deep-dive/src/app/app.component.ts
+++ 3-cmp-deep-dive/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { ServerStatusComponent } from './dashobard/server-status/server-status.component';
-import { TrafficComponent } from "./dashobard/traffic/traffic.component";
-import { TicketsComponent } from './dashobard/tickets/tickets.component';
+import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
+import { TrafficComponent } from "./dashboard/traffic/traffic.component";
+import { TicketsComponent } from './dashboard/tickets/tickets.component';
 
 @Component({
   selector: 'app-root',
