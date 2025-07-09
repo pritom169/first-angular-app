a60d522f485167605b2dcfccfc44eb91e17c5d73
Pritom Mazumder <pritommazumder@gmail.com>
2025-07-09 12:42:30 +0200

    [ADDITION] Maing sense of Component Host Elements

--- 3-cmp-deep-dive/src/app/shared/button/button.component.ts
+++ 3-cmp-deep-dive/src/app/shared/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'button[appButton]',
+  selector: 'button[appButton], a[appButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
@@ -9,4 +9,5 @@ import { Component } from '@angular/core';
 })
 
 export class ButtonComponent {
+
 }
