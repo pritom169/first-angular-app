566e714658726c778eea5d171cf3eff883a4b5dc
Pritom Mazumder <pritommazumder@gmail.com>
2025-07-09 12:20:09 +0200

    [ADDITION] Understanding & Configuring View Encapsulation

--- 3-cmp-deep-dive/src/app/shared/control/control.component.ts
+++ 3-cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -1,12 +1,14 @@
-import { Component, input } from '@angular/core';
+import { Component, ViewEncapsulation, input } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None
 })
+
 export class ControlComponent {
   label = input.required<string>();
 }
