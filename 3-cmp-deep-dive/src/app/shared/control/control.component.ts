eb75bc2e5eeda2265f86da8965df69f48b8c5cd9
Pritom Mazumder <pritommazumder@gmail.com>
2025-07-09 14:06:46 +0200

    [ADDITION] Interacting with Host elements from Inside Components
    As we can see, in the host element we had to write class="control" in the new-ticket component which is the host component of <app-control>.
    
    Problem: For every <app-control> component we have to write "class="control"". It really easy to forget.
    
    Solution: We can remove the "class="control"" and put that attribute inside the host element "@Component" decorator as "host: {
        class: 'control'
      }"
    
    As a result, app-control will render as <app-control class="control"> in the browser.

--- 3-cmp-deep-dive/src/app/shared/control/control.component.ts
+++ 3-cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -6,7 +6,10 @@ import { Component, ViewEncapsulation, input } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control'
+  }
 })
 
 export class ControlComponent {
