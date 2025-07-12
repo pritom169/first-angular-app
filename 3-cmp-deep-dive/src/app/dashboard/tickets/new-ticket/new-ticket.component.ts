fe78d53a6e84b1a90e3ab8d1e4ca84ace6668a48
Pritom Mazumder <pritommazumder@gmail.com>
2025-07-12 17:31:49 +0200

    [ADDITION] Getting Access to Template Element via ViewChild

--- 3-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 3-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
@@ -12,8 +12,16 @@ import { ControlComponent } from "../../../shared/control/control.component";
 })
 
 export class NewTicketComponent {
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  // @ViewChild lets you query a DOM element or Angular component from your template and access it in your component class after view initialization.
+  // 'form' — matches the #form in the template.
+  // Optional (?) means it might not be available at first.
+  // ElementRef<T> — gives you access to the actual DOM element.
+  
   onSubmit(input: string, text: string){
     console.log(input);
     console.log(text);
+
+    this.form?.nativeElement.reset();
   }
 }
