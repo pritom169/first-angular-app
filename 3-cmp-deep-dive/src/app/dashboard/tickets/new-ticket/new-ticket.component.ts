ff76d7e315b22deb607420c350f667d2d4582c7a
Pritom Mazumder <pritommazumder@gmail.com>
2025-07-12 21:13:57 +0200

    [ADDITION] Using the signal based viewChild Function.

--- 3-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 3-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
@@ -12,16 +12,23 @@ import { ControlComponent } from "../../../shared/control/control.component";
 })
 
 export class NewTicketComponent {
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
   // @ViewChild lets you query a DOM element or Angular component from your template and access it in your component class after view initialization.
   // 'form' — matches the #form in the template.
   // Optional (?) means it might not be available at first.
   // ElementRef<T> — gives you access to the actual DOM element.
-  
+
+  private form = viewChild<ElementRef<HTMLFormElement>>('form');
+  // This is a new signal-based ViewChild in Angular 17+ (no decorator needed).
+  // `viewChild` returns a signal function you can call like `this.form()` to get the reference.
+  // Works only after the view is initialized.
+
   onSubmit(input: string, text: string){
     console.log(input);
     console.log(text);
 
-    this.form?.nativeElement.reset();
+    this.form()?.nativeElement.reset();
+    // Use nativeElement to directly call DOM methods like reset().
+    // Safe access with optional chaining (?.) in case it's undefined.
   }
 }
