573c5850148851a4e608f1f2df9ed8455b155c08
Pritom Mazumder <pritommazumder@gmail.com>
2025-07-13 22:20:48 +0200

    [ADDITION] Revisiting Inptus and Signals

--- 3-cmp-deep-dive/src/app/dashboard/tickets/ticket/ticket.component.ts
+++ 3-cmp-deep-dive/src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Ticket } from '../tickets.model';
 
 @Component({
   selector: 'app-ticket',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
-
+  data = input.required<Ticket>();
 }
